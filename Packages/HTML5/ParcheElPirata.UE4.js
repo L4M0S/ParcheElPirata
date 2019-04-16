309087a2acf1bf30611c7ba87ec25c6d9e09a40d
L4M0S <13170563+L4M0S@users.noreply.github.com>
2019-04-16 14:50:45 -0500

    Agregado compilacion visual studio 2017

--- Packages/HTML5/ParcheElPirata.UE4.js
+++ Packages/HTML5/ParcheElPirata.UE4.js
@@ -68,7 +68,7 @@ var Module = {
 	assetDownloadProgress: {}, // Track how many bytes of each needed asset has been downloaded so far.
 
 	UE4_indexedDBName: 'UE4_assetDatabase_ParcheElPirata', // this should be an ascii ID string without special characters that is unique to the project that is being packaged
-	UE4_indexedDBVersion: 201904040039, // Bump this number to invalidate existing IDB storages in browsers.
+	UE4_indexedDBVersion: 201904152227, // Bump this number to invalidate existing IDB storages in browsers.
 };
 
 
--- Packages/HTML5/ParcheElPirata.data.js
+++ Packages/HTML5/ParcheElPirata.data.js
@@ -180,6 +180,6 @@ Module['FS_createPath']('/ParcheElPirata/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"audio": 0, "start": 53, "crunched": 0, "end": 101, "filename": "/UE4CommandLine.txt"}, {"audio": 0, "start": 101, "crunched": 0, "end": 38822311, "filename": "/ParcheElPirata/Content/Paks/ParcheElPirata-HTML5.pak"}], "remote_package_size": 38822311, "package_uuid": "1cafde76-e1a6-409c-b0d7-31e6b7b718e5"});
+ loadPackage({"files": [{"audio": 0, "start": 0, "crunched": 0, "end": 53, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"audio": 0, "start": 53, "crunched": 0, "end": 101, "filename": "/UE4CommandLine.txt"}, {"audio": 0, "start": 101, "crunched": 0, "end": 38896173, "filename": "/ParcheElPirata/Content/Paks/ParcheElPirata-HTML5.pak"}], "remote_package_size": 38896173, "package_uuid": "44cc7c11-3fd6-4856-84a1-71a115b127c7"});
 
 })();
